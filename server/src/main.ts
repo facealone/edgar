40dbfb4787eb237097c756ce55ac14fb35fecf3e
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2019-04-07 12:29:30 +0200

    Add sentry

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,16 @@
 import 'dotenv/config';
+import * as Sentry from '@sentry/node';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './AppModule';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  Sentry.init({ dsn: process.env.SENTRY_DSN });
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(Sentry.Handlers.errorHandler());
 
   const options = new DocumentBuilder()
     .setTitle('Edgar Majordome')
