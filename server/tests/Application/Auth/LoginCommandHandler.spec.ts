b92c6c5b89b8b7fc2c8b1971af3db99c96b3bdbe
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2019-06-27 17:21:37 +0200

    [server] Try unit test

--- server/tests/Application/Auth/LoginCommandHandler.spec.ts
+++ server/tests/Application/Auth/LoginCommandHandler.spec.ts
@@ -1,51 +1,50 @@
+import { Test } from '@nestjs/testing';
 import { LoginCommandHandler } from 'src/Application/Auth/Command/LoginCommandHandler';
+import { UserRepository } from 'src/Infrastructure/User/Repository/UserRepository';
+import { EncryptionAdapter } from 'src/Infrastructure/Adapter/EncryptionAdapter';
 import { User } from 'src/Domain/User/User.entity';
 import { LoginCommand } from 'src/Application/Auth/Command/LoginCommand';
-import { AuthenticatedView } from 'src/Application/Auth/View/AuthenticatedView';
-import { House } from 'src/Domain/House/House.entity';
-import { HouseView } from 'src/Application/House/View/HouseView';
-import * as EncryptionAdapter from 'src/Infrastructure/Adapter/EncryptionAdapter';
-import * as UserRepository from 'src/Infrastructure/User/Repository/UserRepository';
-
-jest.mock('src/Infrastructure/Adapter/EncryptionAdapter');
-jest.mock('src/Infrastructure/User/Repository/UserRepository');
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('[auth] Login', () => {
+  let handler: LoginCommandHandler;
+  let userRepository: UserRepository;
+  let encryptionAdapter: EncryptionAdapter;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        LoginCommandHandler,
+        UserRepository,
+        EncryptionAdapter,
+        { provide: 'IUserRepository', useClass: UserRepository },
+        { provide: 'IEncryptionAdapter', useClass: EncryptionAdapter },
+      ],
+    })
+      .overrideProvider(getRepositoryToken(User))
+      .useValue({
+        findOneByEmail: () => new User({ email: 'mathieu.marchois@gmail.com' }),
+        findOne: () => new User({ email: 'mathieu.marchois@gmail.com' }),
+      })
+      .compile();
+
+    userRepository = module.get<UserRepository>(UserRepository);
+    encryptionAdapter = module.get<EncryptionAdapter>(EncryptionAdapter);
+    handler = module.get<LoginCommandHandler>(LoginCommandHandler);
+  });
+
   it('Should authenticate successfully', async () => {
-    const house = new House({
-      id: '11111111-1111-1111-1111-111111111111',
-      name: 'Boulevard Ney',
-    });
+    const user = new User({ email: 'mathieu.marchois@gmail.com' });
 
-    const user = new User({
-      email: 'mathieu.marchois@gmail.com',
-      firstName: 'Mathieu',
-      lastName: 'MARCHOIS',
-      currentHouse: house,
-    });
+    jest
+      .spyOn(userRepository, 'findOneByEmail')
+      .mockImplementation(() => Promise.resolve(user));
 
-    const command = new LoginCommand();
-    command.email = 'mathieu.marchois@gmail.com';
-    command.password = 'password';
+    jest.spyOn(encryptionAdapter, 'compare').mockImplementation(() => true);
 
-    const expectedResult = new AuthenticatedView(
-      'Mathieu',
-      'MARCHOIS',
-      'mathieu.marchois@gmail.com',
-      'abcdefgToken',
-      new HouseView('11111111-1111-1111-1111-111111111111', 'Boulevard Ney'),
-    );
-
-    const userRepository = <jest.Mock<typeof UserRepository>>UserRepository;
-
-    const encryptionAdapter = EncryptionAdapter.mock.instances[0];
-
-    const handler = new LoginCommandHandler(userRepository, encryptionAdapter);
-
-    expect(await handler.execute(command)).toEqual(expectedResult);
+    expect(await handler.execute(new LoginCommand())).toBe(user);
   });
 
   //it('Should throw an exception when user not found', () => {});
-
   //it('Should throw an exception when user password not match', () => {});
 });
